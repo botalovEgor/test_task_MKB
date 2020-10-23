8765a645ed986b9495ecbff9be416463f1b9efd7
Egor Botalov <botalovegor46@gmail.com>
2020-10-23 18:32:11 +0300

    in process

--- src/main/java/com/mcb/creditfactory/service/CollateralObjectAdapter.java
+++ src/main/java/com/mcb/creditfactory/service/CollateralObjectAdapter.java
@@ -4,17 +4,15 @@ import com.mcb.creditfactory.dto.Collateral;
 import com.mcb.creditfactory.external.CollateralObject;
 import com.mcb.creditfactory.external.CollateralType;
 import com.mcb.creditfactory.model.AssessedValue;
+import lombok.AllArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Comparator;
 
+@AllArgsConstructor
 public class CollateralObjectAdapter implements CollateralObject {
     private Collateral collateral;
-
-    public CollateralObjectAdapter(Collateral collateral) {
-        this.collateral = collateral;
-    }
 
     @Override
     public BigDecimal getValue() {
@@ -37,6 +35,7 @@ public class CollateralObjectAdapter implements CollateralObject {
     }
 
     private AssessedValue getLastAssessedValue(Collateral collateral) {
-        return collateral.getAssessedValues().stream().max(Comparator.comparing(AssessedValue::getEvaluationDate)).get();
+        return collateral.getAssessedValues().stream().max(Comparator.comparing(AssessedValue::getEvaluationDate))
+                .orElseThrow(()->new IllegalArgumentException("value not found"));
     }
 }
