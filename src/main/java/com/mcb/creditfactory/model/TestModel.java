37ab9c9b8c57a81688a47ccf94dd500e38b635b4
Egor Botalov <egor.botalov@rt.ru>
2022-01-14 15:03:10 +0300

    all back

---
+++ src/main/java/com/mcb/creditfactory/model/TestModel.java
@@ -0,0 +1,12 @@
+package com.mcb.creditfactory.model;
+
+public class TestModel {
+    String one;
+    String two;
+    String three;
+    String fors;
+    String five;
+    String six;
+    String seven;
+    String eight;
+}
